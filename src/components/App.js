b2fc574033135fa5e8979a0a4da35dd8d1600b20
valensixx <valensixx.gmail.com>
2023-03-11 13:25:23 +0200

    creating the counter

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
+//create counter using react
 export default function App(){
+    const [counter, setCounter] = useState(0); // this is the states the name of the const 'counter' with set+name of the const. We need to import {useState}
     return (
         <div>
-            <h1>My first component</h1>
+            <div>Counter: {counter}</div>
+            <button onClick={() => {
+                console.log(counter);
+            }}>+</button>
+            <button>-</button>
         </div>
     )
 }
