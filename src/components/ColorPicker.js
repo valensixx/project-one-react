b383302269a8565282032fa199ab0d242bd8f9f3
valensixx <valensixx.gmail.com>
2023-03-11 15:07:09 +0200

    colorPicker component

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,10 @@
+import React, {useState} from "react";
+
+/*Create color picker and put color in a div*/
+export default function App(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 import Button from './components/Button';
+import ColorPicker from './components/ColorPicker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Button/>
+    <ColorPicker />
   </React.StrictMode>
 );
 
