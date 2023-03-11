57872a726a0d1b517b0c07f0cc137086f3516947
valensixx <valensixx.gmail.com>
2023-03-11 12:55:49 +0200

    project-one

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div>Hello world</div>
+  </React.StrictMode>
+);
+
+// git config --global user.email "you@example.com"
+//git config --global user.name "Your Name"
