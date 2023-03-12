076fee290c10eff4eca07902e38d95516ce38e00
valensixx <valensixx.gmail.com>
2023-03-12 16:14:02 +0200

    const color/span/error

--- src/components/RegForm.js
+++ src/components/RegForm.js
@@ -17,7 +17,17 @@ export default function App(){
     const [city, setCity] = useState('');
     const [gender, setGender] = useState('');
     const [music, setMusic] = useState([]);
+    const [errors, setErrors] = useState({
+        name: '',
+        email: '',
+        password: '',
+        city: '',
+        gender: '',
+        music: '',
+    });
  
+    const color = {color:'red'};
+
     return(
         <div>
             <h3>RegForm</h3>
@@ -25,18 +35,22 @@ export default function App(){
                 <div>
                     <label>Name</label>
                     <input type="text" />
+                    <span style={color}>{errors.name}</span>
                 </div>
                 <div>
                     <label>Email</label>
                     <input type="text" />
+                    <span style={color}>{errors.email}</span>
                 </div>
                 <div>
                     <label>Password</label>
                     <input type="password" />
+                    <span style={color}>{errors.password}</span>
                 </div>
                 <div>
                     <label>Repeat Password</label>
                     <input type="password" />
+                    <span style={color}>{errors.password}</span>
                 </div>
                 <div>
                     <label>City</label>
@@ -45,12 +59,14 @@ export default function App(){
                         <option value ="Plovdiv">Plovdiv</option>
                         <option value ="Sofia">Sofia</option>
                     </select>
+                    <span style={color}>{errors.city}</span>
                 </div>
                 <div>
                     <label>Male</label>
                     <input type="radio" name="gender" />
                     <label>Female</label>
                     <input type="radio" name="gender" />
+                    <span style={color}>{errors.gender}</span>
                 </div>
                 <div>
                     <label>Rap</label>
@@ -59,6 +75,7 @@ export default function App(){
                     <input type="checkbox" />
                     <label>POP</label>
                     <input type="checkbox" />
+                    <span style={color}>{errors.music}</span>
                 </div>
                 <div>
                     <button type ="button">Registration</button>
