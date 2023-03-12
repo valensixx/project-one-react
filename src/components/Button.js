4041f7a0f07ce92230ee5dec7e3dd1b53e61d8e9
valensixx <valensixx.gmail.com>
2023-03-12 14:58:26 +0200

    fixied

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,29 +1,45 @@
-import React,{useState} from "react";
-//Create button for turning on and off. I will put color over a text. 
-export default function App(){
-    const [state, setState] = useState('off');
-    return(
-        <div>
-            <h1 style={{
-                color:'blueviolet',
-                fontSize:'2rem',
-            }}>This is task 2 - button turn on and off!
-            </h1>
-            <button 
-                style = {{ // in react as style we are making json object
-                marginTop:'0.2rem',
-                color: state == 'on' ? 'green' : 'red',
-                border: 'unset',
-                fontSize:'2rem',
-            }}
-                onClick={() => {
-                    if(state == 'off'){
-                        setState('on');
-                    }else {
-                        setState('off')
-                    }
-                }}
-            >{state}</button>
-        </div>
+import React from "react";
+//create turn on and off button
+export default class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      background: "black",
+      isOn: false,
+    };
+  }
+
+  handleBackgroundChange = () => {
+    this.setState({
+      isOn: !this.state.isOn,
+      background: this.state.isOn ? "red" : "green",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1
+          style={{
+            color: "blueviolet",
+            fontSize: "2rem",
+          }}
+        >
+          This is task 2 - button turn on and off!
+        </h1>
+        <button
+          style={{
+            marginTop: "0.2rem",
+            color: this.state.isOn ? "green" : "red",
+            backgroundColor: this.state.background,
+            border: "unset",
+            fontSize: "2rem",
+          }}
+          onClick={this.handleBackgroundChange}
+        >
+          Click me!
+        </button>
+      </div>
     );
+  }
 }
