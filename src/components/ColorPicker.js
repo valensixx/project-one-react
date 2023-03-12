a55391b9cc9a899af3696948190ac03bc9fa2ecc
valensixx <valensixx.gmail.com>
2023-03-12 14:49:26 +0200

    done changes in the code with class components

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,10 +1,33 @@
-import React, {useState} from "react";
+import React from "react";
 
 /*Create color picker and put color in a div*/
-export default function App(){
-    return(
-        <div>
-            
-        </div>
-    );
+export default class App extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    state = {
+        background: 'white',
+        text: 'initial text'
+    }
+
+    render(){
+        console.log(this.state);
+        return (
+            <div>
+                <p style={{ marginTop: '2rem', }}>This is task 3 - Color Picker</p>
+                <input type="color" onChange={(e) => this.setState({background: e.target.value})}/>
+                <div style={{
+                    background: this.state.background,
+                    width: 100,
+                    height: 100,
+                    border: '1px solid black',
+                    marginTop: "1rem",
+                }}>
+                <input type="text" onChange={(e) => this.setState({text: e.target.value})}></input>
+                <div>{this.state.text}</div>
+                </div>
+            </div>
+        );
+    }
 }
