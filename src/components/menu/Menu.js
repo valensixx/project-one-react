f9201eaa4567b997e6ab6e80f5ac3f5a68300f6b
valensixx <valensixx@gmail.com>
2023-03-13 18:44:08 +0200

    jsx structure

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,9 +1,21 @@
 import React from "react";
 import './Menu.css';
-export default function Menu(){
+function Menu(){
     return(
-        <nav>
-            <h1>Hello from menu</h1>
+        <nav className="menu-nav">
+            <ul className = "menu-nav-ul"> 
+                <li className="menu-nav-item">
+                    <a href="">Home</a>
+                </li>
+                <li className="menu-nav-item">
+                    <a href="">About us</a>
+                </li>
+                <li className="menu-nav-item">
+                    <a href="">Contacts</a>
+                </li>
+            </ul>
         </nav>
     );
 };
+
+export default Menu;  //this is a second way of doing  export default function Menu(){}
